6fbf807661badb128b1d47b5540699b8f90743f4
Martin Griffin <martinrgriffin@gmail.com>
2026-05-01 06:27:58 +0100

    Un-inline RunBattleScriptCommands

--- src/battle_util.c
+++ src/battle_util.c
@@ -5648,10 +5648,9 @@ void ClearVariousBattlerFlags(enum BattlerId battler)
     gBattleMons[battler].volatiles.grudge = FALSE;
 }
 
-void HandleAction_RunBattleScript(void) // identical to RunBattleScriptCommands
+void HandleAction_RunBattleScript(void)
 {
-    if (gBattleControllerExecFlags == 0)
-        gBattleScriptingCommandsTable[*gBattlescriptCurrInstr]();
+    RunBattleScriptCommands();
 }
 
 u32 SetRandomTarget(enum BattlerId battlerAtk)
